1197c1544099c65e8bd69933559951ccd849e6b6
EricVazquezDJ <eric_25_06@outlook.com>
2019-10-04 22:47:12 -0500

    Lista enlazada con menú (ordenar y eliminar)

--- Nodos.cpp
+++ Nodos.cpp
@@ -11,12 +11,28 @@ struct Nodo{
 		puntero=NULL;
 	}
 	
-	void insertar(int nuevoValor){
+	void retroceder(int nuevoValor){
 		Nodo *actual=puntero;
 		Nodo *nuevoNodo=new Nodo(nuevoValor);
-		if(actual!=NULL) while(actual->puntero!=NULL) actual=actual->puntero;
+		if(actual!=NULL) while(actual->puntero!=NULL)
+		actual=actual->puntero;
 		if(actual==NULL) puntero=nuevoNodo;
 		else actual->puntero=nuevoNodo;
+	}
+	
+	void ordenar(int nuevoValor){
+		Nodo *actual=puntero;
+		Nodo *nuevoNodo= new Nodo(nuevoValor);
+		Nodo *before=this;
+		if(actual==NULL){
+			before->puntero=nuevoNodo;
+			return;
+		}else while(actual!=NULL && nuevoValor>actual->valor){
+			before=actual;
+			actual=actual->puntero;
+		}
+		nuevoNodo->puntero=before->puntero;
+		before->puntero=nuevoNodo;
 	}
 	
 	void mostrar(){
@@ -27,11 +43,31 @@ struct Nodo{
 		}
 	}
 	
+	void PopBack(){
+		Nodo *actual=puntero;
+		Nodo *before=this;
+		if(actual==NULL) return;
+		else while(actual->puntero!=NULL){
+			before=actual;
+			actual=actual->puntero;
+		}
+		before->puntero=NULL;
+		delete actual;
+	}
+	
+	void PopFront(){
+		Nodo *actual=puntero;
+		Nodo *before=this;
+		if(actual==NULL) return;
+		before->puntero=actual->puntero;
+		delete actual;
+	}
+	
 };
 
 	void menu(){
     cout<<"\nMenú de opciones \n"<<endl;
-    cout<<"1 Agregar nodo \n"<<"2 Mostar Valores \n"<<"3 Salir \n"<<endl;
+    cout<<"1 Agregar nodo \n"<<"2 Eliminar dato \n"<<"3 Mostrar lista \n"<<endl;
     cout<<"Elige una opción del menú: \n"<<endl;
 	}
 Nodo l1;
@@ -49,11 +85,13 @@ int main(){
 			int nuevoDato;
 			cout<<"Dato a ingresar: ";
 			cin>>nuevoDato;
-			l1.insertar(nuevoDato);
+			l1.ordenar(nuevoDato);
         }else if(opcion==2){
         	system("CLS");
-        	cout<<"Los valores son: "<<endl;
-        	l1.mostrar();
+        	l1.PopFront();	
+		}else if(opcion==3){
+			system("CLS");
+			l1.mostrar();
 		}else {
 			break;
 		}
